e448be9816bcd0774176a5dd0059f2f036e73083
shinwaris <zoology.fb@gmail.com>
2022-09-26 19:34:56 +0500

    created index view

--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """displays the landing page of the base app"""
+    context = {}
+    return render(request, "base/index.html", context)
