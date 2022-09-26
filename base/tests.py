7f8d8d26b71a6f12f128a63f5c03cf05dc9e64e2
shinwaris <zoology.fb@gmail.com>
2022-09-26 20:06:51 +0500

    creating test cases, requirements file

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 # Create your tests here.
+def TestBaseAppStatus(TestCase):
+    """tests status of all pages"""
+
+    def test_landing_page(self):
+        """tests the status of the landing page"""
+        client = Client()
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
