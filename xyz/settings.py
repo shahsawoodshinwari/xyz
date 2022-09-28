7fbb21ac8d89c21bc5e190ceb708baa8acecc8ec
shinwaris <zoology.fb@gmail.com>
2022-09-28 12:31:02 +0500

    added social context processors, requirements

--- xyz/settings.py
+++ xyz/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "django.template.context_processors.media",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
