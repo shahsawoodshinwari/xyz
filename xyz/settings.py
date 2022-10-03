0ebabcb1d6461391af5094b63d176d4bf838fd38
shinwaris <zoology.fb@gmail.com>
2022-10-03 12:44:58 +0500

    changed allowed hosts

--- xyz/settings.py
+++ xyz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["demo.lastwave.xyz"]
 
 # CSRF_COOKIE_SECURE = True
 # SECURE_SSL_REDIRECT = True
